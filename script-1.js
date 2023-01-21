7ddadc83e9fc2badcfeae8451a90fb8e8bd76e12
Irina Iskra <iskrairina.a@gmail.com>
2023-01-21 13:11:46 +0200

    task3

--- script-1.js
+++ script-1.js
@@ -12,24 +12,60 @@
 // const time = `${hoursMode}:${minutesMode}`;
 // console.log(time);
 
-const userInfo = Number(prompt("Введіть значення від 0 до 60 включно"));
+// const userInfo = Number(prompt("Введіть значення від 0 до 60 включно"));
 
-function checkQuater(num) {
+// function checkQuater(num) {
+//   let message;
+
+//   if (num >= 0 && num <= 15) {
+//     message = console.log("Ваше число в 1ій чверті.");
+//   } else if (num >= 16 && num <= 30) {
+//     message = console.log("Ваше число в 2ій чверті.");
+//   } else if (num >= 31 && num <= 45) {
+//     message = console.log("Ваше число в 3ій чверті.");
+//   } else if (num >= 46 && num <= 60) {
+//     message = console.log("Ваше число в 4ій чверті.");
+//   } else {
+//     message = console.log("Ваше число невірне.");
+//   }
+
+//   return message;
+// }
+
+// checkQuater(userInfo);
+
+function checkString() {
+  const userStr = prompt("Input string");
+  const userLetter = prompt("Write letter");
+  const userLetterNum = Number(prompt("Write letter`s number"));
   let message;
+  // console.log(typeof userStr);
+  // console.log(typeof userLetter);
+  // console.log(typeof userLetterNum);
 
-  if (num >= 0 && num <= 15) {
-    message = console.log("Ваше число в 1ій чверті.");
-  } else if (num >= 16 && num <= 30) {
-    message = console.log("Ваше число в 2ій чверті.");
-  } else if (num >= 31 && num <= 45) {
-    message = console.log("Ваше число в 3ій чверті.");
-  } else if (num >= 46 && num <= 60) {
-    message = console.log("Ваше число в 4ій чверті.");
+  if (
+    typeof userStr === "string" &&
+    typeof userLetter === "string" &&
+    typeof userLetterNum === "number"
+  ) {
+    const arrayStr = userStr.split("");
+
+    if (!arrayStr.includes(userLetter)) {
+      message = `There is no such letter`;
+    } else {
+      if (userLetterNum <= arrayStr.length) {
+        if (arrayStr[userLetterNum - 1] === userLetter) {
+          message = `Your letter ${userLetter} in string ${userStr} on position ${userLetterNum}`;
+        } else {
+          message = `Your letter ${userLetter}  doesn't match position ${userLetterNum}`;
+        }
+      } else {
+        message = "Your LetterNum is false";
+      }
+    }
   } else {
-    message = console.log("Ваше число невірне.");
+    message = "Your data is invalid";
   }
-
-  return message;
+  return console.log(message);
 }
-
-checkQuater(userInfo);
+checkString();
